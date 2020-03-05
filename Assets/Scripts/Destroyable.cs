c91d2932100345f99ec975239ebd509f36d56d31
Michael Muniz <michael.muniz@scifusion360.com>
2020-03-05 16:53:36 -0500

    fix destroyable distance check

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Destroyable : MonoBehaviour
 {
-    const int distance = 2;
+    const float distance = 1.5f;
     public float maxTimer = 1.0f;
     bool counting = false;
     private float timer = 1.0f;
@@ -11,7 +11,7 @@ public class Destroyable : MonoBehaviour
         // check if player is close enough
         // start destruction timer
         Vector2 pos = Camera.main.transform.position;
-        if (pos.x - transform.position.x < distance && pos.x + transform.position.x > -distance && pos.y - transform.position.y < distance && pos.y + transform.position.y > -distance)
+        if (Mathf.Abs(pos.x - transform.position.x) < distance && Mathf.Abs(pos.y - transform.position.y) < distance)
         {
             timer = maxTimer;
             counting = true;
